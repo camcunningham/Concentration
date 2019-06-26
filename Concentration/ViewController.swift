c4f22668badb11b41ac8427a2db5fa0774493c98
Cam Cunningham <16cjc12@queensu.ca>
2019-06-25 23:11:33 -0400

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Cam Cunningham on 2019-06-25.
+//  Copyright © 2019 Cam Cunningham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
